0a5b7dbf7f054b8dd4e27bc25dfda1e4b5950e75
Junli <diaopkaique@gmail.com>
2017-05-08 00:11:42 +0000

    update test_image2.py

--- test_image2.py
+++ test_image2.py
@@ -1,4 +1,22 @@
-from PIL import Image
+import Tkinter as tk
+from PIL import ImageTk, Image
 
-img = Image.open('image.jpg')
-img.rotate(45).show()
+#This creates the main window of an application
+window = tk.Tk()
+window.title("Join")
+window.geometry("300x300")
+window.configure(background='grey')
+
+path = "image.jpg"
+
+#Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
+img = ImageTk.PhotoImage(Image.open(path))
+
+#The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+panel = tk.Label(window, image = img)
+
+#The Pack geometry manager packs widgets in rows or columns.
+panel.pack(side = "bottom", fill = "both", expand = "yes")
+
+#Start the GUI
+window.mainloop()
