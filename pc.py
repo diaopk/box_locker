a3709e0c1b77834f2d9ae6144d4bb25520529c9a
junli <diaopkaique@gmail.com>
2017-05-08 10:39:54 +0100

    update pc.py

--- pc.py
+++ pc.py
@@ -10,7 +10,21 @@ import datetime
 camera = PiCamera()
 
 # A Method to take a photo
-def photor():
+def photor1():
     camera.capture('./test_picamera/image-'+str(datetime.datetime.now())+'.jpg')
     print 'Sorry we captured you!'
+
+# Object photor
+class Photor():
+    def __init__(self, p, d):
+        self.path = p
+        self.datetime = d
+
+    def take_photo():
+        camera.capture('./test_picamera/-'+str(self.datetime)+'.jpg')
+        print 'Sorry we captured you!'
+
+    def get_datetime():
+        return self.get_datetime
+
 
